110eb914c50704990f8708f89c6f1c988213cebc
giovi <giovi@10.25.96.217>
2022-10-07 12:23:49 +0200

    rimossi dei warning, niente de che

--- src/main/Controllore.java
+++ src/main/Controllore.java
@@ -1,8 +1,6 @@
 package main;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.lang.reflect.*;
 import java.util.HashMap;
 import java.util.Map;
 
